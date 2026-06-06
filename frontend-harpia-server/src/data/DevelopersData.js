f566cd4995e87aa90efd7c3c734bca30eaa3fba0
Andre R. F. Batista <andrerfbatista@gmail.com>
2026-06-06 16:34:46 -0300

    NEW: extract developers data into separate file

---
+++ frontend-harpia-server/src/data/DevelopersData.js
@@ -0,0 +1,44 @@
+import andrePhoto from "../assets/people/andre.jpeg";
+import andreReisPhoto from "../assets/people/andre_reis.jpeg";
+import marceloPhoto from "../assets/people/marceloNovais.jpeg";
+
+export const developers = [
+
+    // Template para devs e contribuidores futuros
+    // {
+    //     name:
+    //     photo:
+    //     linkedin:
+    //     github:
+    //     email:
+    //     description:
+    // },
+
+    {
+        name: "Andre Rodrigues de Freitas Batista",
+        photo: andrePhoto,
+        linkedin: "https://www.linkedin.com/in/andrefdr/",
+        github: "https://github.com/AndreRFBaT",
+        email: "andrerfbatista@gmail.com",
+        description:
+            "Desenvolvedor de Software | Bacharelado em Engenharia de Computação (em andamento) | Pesquisador de Iniciação Científica na IFSC/USP | Desenvolvimento de Software e Boas Práticas de Engenharia"
+    },
+    {
+        name: "André Felipe Dos Reis",
+        photo: andreReisPhoto,
+        linkedin: "https://www.linkedin.com/in/andrefdr/",
+        github: "https://github.com/andrefdrx",
+        email: "andrefdrx@gmail.com",
+        description:
+            "Bacharel em Ciências Econômicas, Bacharel em Tecnologia da Informação, Graduando em Engenharia da Computação e pós-graduando em Gestão de Projetos. Gestão de Projetos | Compras | Testes de Software |Quality Assurance"
+    },
+    {
+        name: "Marcelo Novais",
+        photo: marceloPhoto,
+        linkedin: "https://www.linkedin.com/in/marcelo-novais-b61634219/",
+        // github: "https://github.com/marcelo-novais",
+        // email: "marcelonovais@gmail.com",
+        description:
+            "Gestão de TI | MBA em Gestão de Logística e Operações | Pós-graduado em Gestão de Pessoas e Liderança | Gestão de Contratos | Tecnologia, Processos e Operações"
+    }
+];
