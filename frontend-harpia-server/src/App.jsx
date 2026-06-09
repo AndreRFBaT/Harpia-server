05a5f5809cea75e571ec029b7e4562e9f4efaf79
Andre R. F. Batista <andrerfbatista@gmail.com>
2026-06-09 09:48:39 -0300

    NEW: Add floating nav to app jsx

--- frontend-harpia-server/src/App.jsx
+++ frontend-harpia-server/src/App.jsx
@@ -1,16 +1,21 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 
 import Home from "./pages/Home/Home";
 import LiveStations from "./pages/LiveStations/LiveStations";
+import FloatingNav from "./components/FloatingNav/FloatingNav";
 
 function App() {
+
+  const location = useLocation();
+
   return (
     <>
       <Header />
-
+      {/* <FloatingNav />  Add se for necessário para todas as páginas*/}
+      {location.pathname === "/" && <FloatingNav />} {/* Exibe apenas na Home */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/live-stations" element={<LiveStations />} />
